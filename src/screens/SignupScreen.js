6e9673cd150576181bf12302abb4200a79ec1368
devcihat <cihattsungu@outlook.com>
2021-05-19 09:46:52 +0300

    Create Tracks

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from "react-native"
+
+const SignupScreen = ({ navigation }) => {
+    return (
+        <>
+        <Text style={{ fontSize:48 }}>SignupScreen</Text>
+        <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')} />
+        </>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default SignupScreen;
